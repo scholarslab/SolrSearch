0b84e0b0b4c42ab1973d7848b35504a407a2159f
wsg4w <wsg4w@d187d172-3112-0410-8ee7-ad866068e984>
2009-10-07 14:57:02 +0000

    Unit test stub
    git-svn-id: https://addons.omeka.org/svn/plugins/SolrSearch/trunk@1631 d187d172-3112-0410-8ee7-ad866068e984

---
+++ tests/SolrSearch/SolrSearchTest.php
@@ -0,0 +1,17 @@
+<?php
+class SolrSearch_SolrSearchTest extends Omeka_Model_TestCase
+{
+	protected $_search;
+	
+	public function setUp()
+	{
+		parent::setUp();
+	}
+	
+	public function tearDown()
+	{
+		parent::tearDown();
+	}
+}
+
+?>
