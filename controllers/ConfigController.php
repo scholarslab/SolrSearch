355cf2cb1bb53beea1df48cee688fb05e11ca67a
ewg4x <ewg4x@d187d172-3112-0410-8ee7-ad866068e984>
2010-09-14 15:15:48 +0000

    removed unused highlight functions from configcontroller
    git-svn-id: https://addons.omeka.org/svn/plugins/SolrSearch/trunk@3184 d187d172-3112-0410-8ee7-ad866068e984

--- controllers/ConfigController.php
+++ controllers/ConfigController.php
@@ -7,11 +7,6 @@ class SolrSearch_ConfigController extends Omeka_Controller_Action
 		$this->view->form = $form;
 	}
 	
-	public function highlightAction(){
-		$form = $this->highlightForm();
-		$this->view->form = $form;
-	}
-	
 	public function updateAction()
 	{
 		$form = $this->facetForm();
@@ -63,64 +58,6 @@ class SolrSearch_ConfigController extends Omeka_Controller_Action
     	}	
 	}
 	
-	public function updateHighlightAction()
-	{
-		$form = $this->highlightForm();
-		
-		if ($_POST) {
-    		if ($form->isValid($this->_request->getPost())) {    
-    			//get posted values		
-				$uploadedData = $form->getValues();
-				set_option('solr_search_hl', $uploadedData['solr_search_hl']);
-				set_option('solr_search_snippets', $uploadedData['solr_search_snippets']);
-				set_option('solr_search_fragsize', $uploadedData['solr_search_fragsize']);
-				
-    		}
-	    	else {
-	    			$this->flashError('Failed to gather posted data.');
-	    			$this->view->form = $form;
-	    	}
-    	}	
-	}
-	
-	private function highlightForm(){
-		require "Zend/Form/Element.php";
-	    $form = new Zend_Form();
-		$form->setAction('updatehighlight');    	
-	    $form->setMethod('post');
-	    $form->setAttrib('enctype', 'multipart/form-data');
-	    
-	    //set true or false
-		$hl = new Zend_Form_Element_Select ('solr_search_hl');
-    	$hl->setLabel('Highlighting:');
-		$hl->addMultiOption('true', 'True');
-		$hl->addMultiOption('false', 'False'); 
-	    $hl->setValue(get_option('solr_search_hl'));
-	    $form->addElement($hl);
-	    
-	    //number of snippets
-		$snippets = new Zend_Form_Element_Text ('solr_search_snippets');
-	    $snippets->setLabel('Snippets:');
-	    $snippets->setValue(get_option('solr_search_snippets'));
-	    $snippets->setRequired('true');    
-		$snippets->addValidator(new Zend_Validate_Int());
-	    $form->addElement($snippets);
-
-		//fragment size
-	    $fragsize = new Zend_Form_Element_Text ('solr_search_fragsize');
-	    $fragsize->setLabel('Fragment Size:');
-	    $fragsize->setValue(get_option('solr_search_fragsize'));
-	    $fragsize->setRequired('true');    
-	    $fragsize->addValidator(new Zend_Validate_Int());
-	    $form->addElement($fragsize);
-	    
-	    //Submit button
-    	$form->addElement('submit','submit');
-    	$submitElement=$form->getElement('submit');
-    	$submitElement->setLabel('Submit');
-	    return $form;
-	}
-
 	private function facetForm() {
 		    require "Zend/Form/Element.php";
 	    	$form = new Zend_Form();
@@ -183,5 +120,3 @@ class SolrSearch_ConfigController extends Omeka_Controller_Action
 	    	return $form;
 	}
 }
-
-
