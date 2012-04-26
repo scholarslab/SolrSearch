8165d8df070e572da7f9e351725e973a46ff79db
Eric Rochester <err8n@virginia.edu>
2012-04-26 09:56:01 -0400

    Fixed type in compass configuration.

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'susy'
 http_path = "/"
 css_dir = "views/shared/css"
 sass_dir = "_sass"
-images_dir = "views/hared/images"
+images_dir = "view/shared/images"
 javascripts_dir = "views/shared/javascripts"
 output_style = :compressed # expanded, nested, compact, or compressed
 relative_assets = false
