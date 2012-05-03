e8e9b6af808823b41a40e1a894d416a74152456e
Eric Rochester <err8n@virginia.edu>
2012-05-03 15:03:35 -0400

    Added properties to SolrSearch_Addon_Addon.

--- lib/SolrSearch/Addon/Addon.php
+++ lib/SolrSearch/Addon/Addon.php
@@ -29,10 +29,96 @@
  **/
 class SolrSearch_Addon_Addon
 {
-    
-    function __construct()
-    {
-        // code...
+    //{{{Properties
+
+    /**
+     * The name of the addon.
+     *
+     * @var string
+     **/
+    var $name;
+
+    /**
+     * The value for the result-type facet.
+     *
+     * @var string|null
+     **/
+    var $resultType;
+
+    /**
+     * The name of the table. This doesn't need to have the prefix.
+     *
+     * @var string
+     **/
+    var $table;
+
+    /**
+     * The ID column for this table. This defaults to 'id'.
+     *
+     * @var string
+     **/
+    var $idColumn;
+
+    /**
+     * If this is the child in a hierarchy, this is the parent. If this is set, 
+     * this object should appear in the parent's children property.
+     *
+     * @var SolrSearch_Addon_Addon|null
+     **/
+    var $parentAddon;
+
+    /**
+     * The database key to use to get from this object to the parent.
+     *
+     * @var string|null
+     **/
+    var $parentKey;
+
+    /**
+     * Does this type implement Taggable?
+     *
+     * @var bool
+     **/
+    var $tagged;
+
+    /**
+     * If set, this is a field that acts as a visible flag. If it is set to 
+     * TRUE, then the item should be indexed. Otherwise, it will be skipped.
+     *
+     * @var string|null
+     **/
+    var $flag;
+
+    /**
+     * An array list of fields in this.
+     *
+     * @var array of SolrSearch_Addon_Field
+     **/
+    var $fields;
+
+    /**
+     * An array list of children of this addon.
+     *
+     * @var array of SolrSearch_Addon_Addon
+     **/
+    var $children;
+
+    //}}}
+
+    function __construct(
+        $name=null, $resultType=null, $table=null, $idColumn='id', 
+        $parentAddon=null, $parentKey=null, $tagged=false, $flag=null
+    ) {
+        $this->name        = $name;
+        $this->resultType  = $resultType;
+        $this->table       = $table;
+        $this->idColumn    = $idColumn;
+        $this->parentAddon = $parentAddon;
+        $this->parentKey   = $parentKey;
+        $this->tagged      = $tagged;
+        $this->flag        = $flag;
+        $this->fields      = array();
+        $this->children    = array();
     }
 }
 
