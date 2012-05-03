ecfd82297fc769de89a8dfade585a495a8ccd9c4
Eric Rochester <err8n@virginia.edu>
2012-05-03 14:36:27 -0400

    Add a sub for SolrSearch_Addon_Addon.

---
+++ lib/SolrSearch/Addon/Addon.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * SolrSearch Omeka Plugin helpers.
+ *
+ * Default helpers for the SolrSearch plugin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * @package    omeka
+ * @subpackage SolrSearch
+ * @author     "Scholars Lab"
+ * @copyright  2010 The Board and Visitors of the University of Virginia
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @version    $Id$
+ * @link       http://www.scholarslab.org
+ *
+ * PHP version 5
+ *
+ */
+
+/**
+ * This contains the data for the main information for an addon.
+ **/
+class SolrSearch_Addon_Addon
+{
+    
+    function __construct()
+    {
+        // code...
+    }
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
