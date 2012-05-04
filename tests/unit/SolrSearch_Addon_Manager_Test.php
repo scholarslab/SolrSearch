a2c7a26d3b8b61f458716c62e9a0f357babb1e60
Eric Rochester <err8n@virginia.edu>
2012-05-03 21:24:27 -0400

    Added stub class for tests for SolrSearch_Addon_Manager.

---
+++ tests/unit/SolrSearch_Addon_Manager_Test.php
@@ -0,0 +1,17 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * @package     omeka
+ * @subpackage  SolrSearch
+ * @author      Scholars' Lab <>
+ * @author      David McClure <david.mcclure@virginia.edu>
+ * @copyright   2012 The Board and Visitors of the University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+ */
+
+class SolrSearch_Addon_Manager_Test extends SolrSearch_Test_AppTestCase
+{
+    // TODO
+}
+
