b59d92135799f589a4b142f3acc68bdbbca04d77
Eric Rochester <err8n@virginia.edu>
2012-05-07 15:11:11 -0400

    Added tests for SolrSearch_Addon_Manager.

--- tests/unit/SolrSearch_Addon_Manager_Test.php
+++ tests/unit/SolrSearch_Addon_Manager_Test.php
@@ -5,6 +5,7 @@
  * @package     omeka
  * @subpackage  SolrSearch
  * @author      Scholars' Lab <>
+ * @author      Eric Rochester <erochest@virginia.edu>
  * @author      David McClure <david.mcclure@virginia.edu>
  * @copyright   2012 The Board and Visitors of the University of Virginia
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
@@ -12,6 +13,28 @@
 
 class SolrSearch_Addon_Manager_Test extends SolrSearch_Test_AppTestCase
 {
-    // TODO
+
+    public function setUp()
+    {
+        $this->setUpPlugin();
+        $this->config = new SolrSearch_Addon_Config($this->db);
+    }
+
+    public function testAddonDir()
+    {
+        $mgr = new SolrSearch_Addon_Manager();
+        $this->assertEquals(
+            realpath(SOLR_SEARCH_PLUGIN_DIR . '/addons'),
+            $mgr->addonDir
+        );
+    }
+
+    public function testParseAll()
+    {
+        $mgr = new SolrSearch_Addon_Manager();
+        $mgr->parseAll($this->config);
+        $this->assertCount(4, $mgr->addons);
+    }
+
 }
 
