253c99d86ff72ef1b7897800b75e45b9fa956cc3
Wayne Graham <wayne.graham@virginia.edu>
2012-05-09 16:08:15 -0400

    updates the messages; removes debug info

--- views/shared/javascripts/solrsearch-1.0.0-alpha.js
+++ views/shared/javascripts/solrsearch-1.0.0-alpha.js
@@ -732,16 +732,15 @@ jQuery(function($) {
   var $container = $('#results');
 
   $container.infinitescroll({
-    debug: true,
+    //debug: true,
     animate: true,
     nextSelector: '#solr-nav a.next',
     navSelector: '#solr-nav',
     itemSelector: '.item',
-    msgText: '<em>Loading next set of items...</em>',
-    finishedMsg: '<em>You have reached the end of the results.</em>'
-  }, function(newElements) {
-    window.console && console.log('context: ', this);
-    window.console && console.log('returned: ', newElements);
+    loading: {
+      msgText: '<em>Loading next set of items...</em>',
+      finishedMsg: '<em>You have reached the end of the results.</em>'
+    }
   });
 
 });
