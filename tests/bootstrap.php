2accbac1a3becf28d4ba4a5321b93f344bf397df
Wayne Graham <wayne.graham@virginia.edu>
2012-05-14 12:08:19 -0400

    Update the test runners to use a bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!($omekaDir = getenv('OMEKA_DIR'))) {
+    $omekaDir = dirname(dirname(dirname(dirname(__FILE__))));
+}
+
+require_once $omekaDir . '/application/tests/bootstrap.php';
+require_once 'SolrSearch_Test_AppTestCase.php';
