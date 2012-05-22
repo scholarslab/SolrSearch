e60c2ba52e71203d436b18239dba65323d33ed11
Eric Rochester <err8n@virginia.edu>
2012-05-22 16:36:21 -0400

    Configured the Jasmine specs.

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -1,9 +1,3 @@
 beforeEach(function() {
-  this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    }
-  });
+
 });
