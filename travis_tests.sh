e21c40e48a7ae230a5bef31176775e70279004e3
Wayne Graham <wayne.graham@virginia.edu>
2012-08-31 14:08:49 -0400

    print the jetty.xml file

--- travis_tests.sh
+++ travis_tests.sh
@@ -9,11 +9,16 @@ export OMEKA_DIR=`pwd`/omeka
   echo "omeka_dir set"
 fi
 
+SOLR_VERSION="3.6.1"
+
 echo "Plugin Directory: $PLUGIN_DIR"
 echo "Omeka Directory: $OMEKA_DIR"
 
 echo "\n Starting up Solr..."
 cd $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example && java -Dsolr.solr.home=$PLUGIN_DIR/solr-home -jar start.jar > /dev/null 2>&1
+
+echo "\n dumping jetty.xml"
+cat $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example/etc/jetty.xml
 
 echo "\n Starting tests..."
 cd tests/ && phpunit --configuration phpunit_travis.xml --coverage-text
