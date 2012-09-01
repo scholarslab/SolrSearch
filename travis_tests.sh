13e32f906029d5598713793afd533acb6e05e4c3
Wayne Graham <wayne.graham@virginia.edu>
2012-09-01 09:14:03 -0400

    nohup output and push everything to dev/null

--- travis_tests.sh
+++ travis_tests.sh
@@ -10,15 +10,26 @@ export OMEKA_DIR=`pwd`/omeka
 fi
 
 SOLR_VERSION="3.6.1"
+JETTY_HOME="$PLUGIN_DIR/apache-solr-$SOLR_VERSION/example"
+JETTY_CONSOLE="/dev/null"
+JETTY_ARGS=""
+JETTY_PORT="8080"
+JAVA_OPTIONS="-Djetty.port=8080 -Dsolr.solr.home=$PLUGIN_DIR/solr-home"
+
+RUN_CMD="cd $JETTY_HOME && java $JAVA_OPTIONS -jar $JETTY_HOME/start.jar $JETTY_ARGS"
 
 echo "Plugin Directory: $PLUGIN_DIR"
 echo "Omeka Directory: $OMEKA_DIR"
 
 echo "\n Starting up Solr..."
-cd $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example && java -Djetty.logs=/tmp/jetty.log -Dsolr.solr.home=$PLUGIN_DIR/solr-home -jar start.jar >> /dev/null 2>&1 & 
+#cd $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example && java -Djetty.logs=/tmp/jetty.log -Dsolr.solr.home=$PLUGIN_DIR/solr-home -jar start.jar >> /dev/null 2>&1 & 
 
-echo "\n dumping jetty.xml"
-cat $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example/etc/jetty.xml
+function startJetty {
+  nohup sh -c "exec $RUN_CMD >>$JETTY_CONSOLE 2>&1" >/dev/null &
+}
+#cat $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example/etc/jetty.xml
 
-echo "\n Starting tests..."
-cd tests/ && phpunit --configuration phpunit_travis.xml --coverage-text
+startJetty
+echo $RUN_CMD
+echo "Starting tests..."
+#cd tests/ && phpunit --configuration phpunit_travis.xml --coverage-text
