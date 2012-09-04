46e0f0e71bd4745b246f463046a5dd626db597ae
Wayne Graham <wayne.graham@virginia.edu>
2012-09-04 11:20:10 -0400

    Dump the nohup file

--- travis_tests.sh
+++ travis_tests.sh
@@ -5,8 +5,7 @@ if [ -z $PLUGIN_DIR ]; then
 fi
 
 if [ -z $OMEKA_DIR ]; then
-export OMEKA_DIR=`pwd`/omeka
-  echo "omeka_dir set"
+  OMEKA_DIR=`pwd`/omeka
 fi
 
 SOLR_VERSION="3.6.1"
@@ -17,10 +16,11 @@ JETTY_PORT="8080"
 JAVA_OPTIONS="-Djetty.port=8080 -Dsolr.solr.home=$PLUGIN_DIR/solr-home"
 
 RUN_CMD="cd $JETTY_HOME && java $JAVA_OPTIONS -jar start.jar $JETTY_ARGS"
+#RUN_CMD="java $JAVA_OPTIONS -jar $JETTY_HOME/start.jar $JETTY_ARGS"
 
 function startJetty {
-  echo $RUN_CMD
-  `$RUN_CMD &`
+  #echo $RUN_CMD
+  nohup sh -c "$RUN_CMD" &
   #nohup sh -c "exec $RUN_CMD >>$JETTY_CONSOLE 2>&1" >/dev/null &
 }
 
@@ -31,6 +31,8 @@ echo "Starting up Solr..."
 #cd $PLUGIN_DIR/apache-solr-$SOLR_VERSION/example && java -Djetty.logs=/tmp/jetty.log -Dsolr.solr.home=$PLUGIN_DIR/solr-home -jar start.jar >> /dev/null 2>&1 & 
 
 startJetty
+
+cat nohup.out
 
 echo "Starting tests..."
 #curl "http://localhost:8080/solr/admin/ping"
