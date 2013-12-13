ed3cc33d18cd45422dff94160391ef7845bb89ff
David McClure <david.mcclure@virginia.edu>
2013-12-13 13:31:18 -0800

    Formatting cleanup in the _subform.php partial.

--- views/admin/config/_subform.php
+++ views/admin/config/_subform.php
@@ -13,13 +13,20 @@
 
 <tr>
   <td class="element">
-  <input name='<?php echo $facetId->getFullyQualifiedName() ?>' type='hidden' value='<?php echo $facetId->getValue() ?>' />
-  <div id='<?php echo $id ?>' class='facetlabel'><?php echo $label->getValue() ?></div>
-<script type="text/javascript">
-tip(
-    '#<?php echo $id ?>',
-    '<?php echo $label->getFullyQualifiedName() ?>',
-    '<?php echo $label->getAttrib('revertto') ?>'
-);
-</script>
-</td>
+
+  <input
+    name="<?php echo $facetId->getFullyQualifiedName() ?>"
+    value="<?php echo $facetId->getValue() ?>"
+    type="hidden" />
+
+  <div id="<?php echo $id ?>" class="acetlabel"><?php echo $label->getValue() ?></div>
+
+  <script type="text/javascript">
+    tip(
+      '#<?php echo $id ?>',
+      '<?php echo $label->getFullyQualifiedName() ?>',
+      '<?php echo $label->getAttrib('revertto') ?>'
+    );
+  </script>
+
+  </td>
