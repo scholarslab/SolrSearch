f6a2d523693b44485e9fc6bc43f49e82433aa9d4
David McClure <david.mcclure@virginia.edu>
2014-02-24 11:34:56 -0800

    Adds collection faceting tests.

--- tests/phpunit/cases/SolrSearch_Case_Default.php
+++ tests/phpunit/cases/SolrSearch_Case_Default.php
@@ -440,4 +440,69 @@ SQL
     }
 
 
+    /**
+     * Get a facet URL for use in XPath queries.
+     *
+     * @param string $field The field name.
+     * @param string $value The facet value.
+     * @param string The XPath-compliant URL.
+     */
+    protected function _getFacetLink($field, $value)
+    {
+        return htmlspecialchars_decode(
+            SolrSearch_Helpers_Facet::addFacet($field, $value)
+        );
+    }
+
+
+    /**
+     * Assert the presence of a facet link.
+     *
+     * @param string $url The facet URL.
+     * @param string $text The facet value.
+     */
+    protected function _assertFacetLink($url, $value)
+    {
+        $this->assertXpathContentContains(
+            "//a[@href='$url'][@class='facet-value']", $value
+        );
+    }
+
+
+    /**
+     * Assert the absence of a facet link.
+     *
+     * @param string $url The facet URL.
+     */
+    protected function _assertNotFacetLink($url)
+    {
+        $this->assertNotXpath("//a[@href='$url'][@class='facet-value']");
+    }
+
+
+    /**
+     * Assert the presence of a result link.
+     *
+     * @param string $url The result URL.
+     * @param string $title The result title.
+     */
+    protected function _assertResultLink($url, $title)
+    {
+        $this->assertXpathContentContains(
+            "//a[@href='$url'][@class='result-title']", $title
+        );
+    }
+
+
+    /**
+     * Assert the absence of a result link.
+     *
+     * @param string $url The result URL.
+     */
+    protected function _assertNotResultLink($url)
+    {
+        $this->assertNotXpath("//a[@href='$url'][@class='result-title']");
+    }
+
+
 }
