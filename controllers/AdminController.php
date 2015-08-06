3e219e840578354f3637c01bd6997ab81ed501bd
Eric Rochester <erochest@virginia.edu>
2015-08-06 14:27:39 +0000

    Allow exclusion on private collections.
    This is only enabled when 'Display Private Items' is selected.
    
    Fixes #122.

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -98,7 +98,13 @@ class SolrSearch_AdminController
     protected function _collectionsForm()
     {
         $ctable      = $this->_helper->db->getTable('Collection');
-        $collections = $ctable->findBy(array('public' => 1));
+        $private     = (int)get_option('solr_search_display_private_items');
+
+        if ($private) {
+            $collections = $ctable->findAll();
+        } else {
+            $collections = $ctable->findBy(array('public' => 1));
+        }
 
         $form = new Zend_Form();
         $form->setAction(url('solr-search/collections'))->setMethod('post');
